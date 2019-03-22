7298e77dd900451ee9f6959293cb708280202813
Jason Bryant <jbryant32>
2019-03-21 21:53:26 -0700

    INIT

---
+++ PublicWitnessingApi/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PublicWitnessingApi.Controllers
+{
+    [ApiController]
+    [Route("api/admin")]
+    public class AdminController
+    {
+        public AdminController()
+        {
+
+        }
+
+        [HttpPost("new publisher")]
+        public ActionResult Post([FromBody] object body) {
+
+            return new OkResult();
+        }
+    }
+}
---
+++ PublicWitnessingApi/Repository/DatabaseContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PublicWitnessingApi.Repository
+{
+    public class DatabaseContext
+    {
+        public string connectionString { get; set; }
+        public DatabaseContext()
+        {
+
+        }
+        public async Task  Add(object user)
+        {
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+                var parameters = new List<SqlParameter>() {
+                    new SqlParameter("@Email", "Jbryant@havenstudios.com"),
+                    new SqlParameter("@FirstName", "Jason"),
+                    new SqlParameter("@LastName","Bryant"),
+                    new SqlParameter("@KingdomHall","Panorama"),
+                    new SqlParameter("@Street","184 Paseo Bravo"),
+                    new SqlParameter("@City","Palm Desert"),
+                    new SqlParameter("@State","CA"),
+                    new SqlParameter("@Zip","92211")
+                }.ToArray();
+                var sp = "add_new_users";
+                var Command = new SqlCommand(sp, connection);
+                Command.CommandType = System.Data.CommandType.StoredProcedure;
+                Command.Parameters.AddRange(parameters);
+                int Rows =Command.ExecuteNonQuery();
+            }
+        }
+    }
+}
